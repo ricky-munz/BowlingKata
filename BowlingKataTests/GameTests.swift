60d8638f2f4cce19758d57960fe3439a8ee08fcc
Ricky Munz <ricky.munz@gmail.com>
2024-11-22 09:38:59 -0500

    Extract roll many helper.

--- BowlingKataTests/GameTests.swift
+++ BowlingKataTests/GameTests.swift
@@ -9,19 +9,32 @@ import XCTest
 @testable import BowlingKata
 
 final class GameTests: XCTestCase {
+    private var game: Game!
+
+    override func setUp() {
+        super.setUp()
+        game = Game()
+    }
+
+    override func tearDown() {
+        game = nil
+        super.tearDown()
+    }
+
     func testGutterGame() {
-        let game = Game()
-        for _ in 1...20 {
-            game.roll(0)
-        }
+        rollMany(pins: 0, times: 20)
         XCTAssertEqual(game.score(), 0)
     }
 
     func testAllOnes() {
-        let game = Game()
-        for _ in 1...20 {
-            game.roll(1)
-        }
+        rollMany(pins: 1, times: 20)
         XCTAssertEqual(game.score(), 20)
+    }
+
+    // MARK: - Helpers
+    private func rollMany(pins: Int, times: Int) {
+        for _ in 1...times {
+            game.roll(pins)
+        }
     }
 }
