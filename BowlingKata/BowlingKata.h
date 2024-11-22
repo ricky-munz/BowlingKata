d5cface1dc70c1ebdc46fa91b72c440f7fc39007
Ricky Munz <ricky.munz@gmail.com>
2024-11-22 08:46:31 -0500

    Initial Commit

---
+++ BowlingKata/BowlingKata.h
@@ -0,0 +1,18 @@
+//
+//  BowlingKata.h
+//  BowlingKata
+//
+//  Created by Ricky Munz on 11/22/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BowlingKata.
+FOUNDATION_EXPORT double BowlingKataVersionNumber;
+
+//! Project version string for BowlingKata.
+FOUNDATION_EXPORT const unsigned char BowlingKataVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BowlingKata/PublicHeader.h>
+
+
---
+++ BowlingKataTests/BowlingKataTests.swift
@@ -0,0 +1,13 @@
+//
+//  BowlingKataTests.swift
+//  BowlingKataTests
+//
+//  Created by Ricky Munz on 11/22/24.
+//
+
+import XCTest
+@testable import BowlingKata
+
+final class BowlingKataTests: XCTestCase {
+
+}
