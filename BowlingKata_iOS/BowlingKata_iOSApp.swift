4d257b4cd5498b3d1b8ce75fe4bc1f71493c3c0a
Ricky Munz <ricky.munz@gmail.com>
2024-12-12 09:02:45 -0500

    Add iOS target

---
+++ BowlingKata_iOS/BowlingKata_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  BowlingKata_iOSApp.swift
+//  BowlingKata_iOS
+//
+//  Created by Ricky Munz on 12/12/24.
+//
+
+import SwiftUI
+
+@main
+struct BowlingKata_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BowlingKata_iOSTests/BowlingKata_iOSTests.swift
@@ -0,0 +1,12 @@
+//
+//  BowlingKata_iOSTests.swift
+//  BowlingKata_iOSTests
+//
+//  Created by Ricky Munz on 12/12/24.
+//
+
+import Testing
+@testable import BowlingKata_iOS
+
+struct BowlingKata_iOSTests {
+}
