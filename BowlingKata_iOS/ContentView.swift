3d46c37dcb069997e34409c8148d52cefab0e693
Ricky Munz <ricky.munz@gmail.com>
2024-12-26 09:19:22 -0500

    Add scoring grid

--- BowlingKata_iOS/ContentView.swift
+++ BowlingKata_iOS/ContentView.swift
@@ -10,12 +10,35 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Grid(horizontalSpacing: 0) {
+                GridRow {
+                    ForEach(0..<10) { _ in
+                        ZStack {
+                            Color(.clear)
+                                .border(.black)
+
+                            VStack {
+                                HStack(spacing: 0) {
+                                    ForEach(0..<3) { _ in
+                                        Color(.clear)
+                                            .border(.black)
+                                            .aspectRatio(contentMode: .fit)
+                                    }
+                                }
+                                .frame(maxWidth: .infinity, alignment: .trailing)
+
+                                Spacer()
+                                Spacer()
+                            }
+                            .frame(maxHeight: .infinity, alignment: .top)
+                        }
+                        .scaledToFill()
+                        .gridCellUnsizedAxes(.vertical)
+                    }
+                }
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
