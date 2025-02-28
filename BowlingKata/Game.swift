2975ad3515e1e0e663c22a02d908a79ea2f3ad59
jirving887 <jonathanirving10@gmail.com>
2025-02-28 09:32:06 -0500

    Make Game public

--- BowlingKata/Game.swift
+++ BowlingKata/Game.swift
@@ -7,16 +7,21 @@
 
 import Foundation
 
-final class Game {
-    private var rolls = [Int](repeating: 0, count: 21)
-    private var currentRoll = 0
-
-    func roll(_ pins: Int) {
+public final class Game {
+    private var rolls: [Int]
+    private var currentRoll: Int
+    
+    public init() {
+        rolls = [Int](repeating: 0, count: 21)
+        currentRoll = 0
+    }
+    
+    public func roll(_ pins: Int) {
         rolls[currentRoll] = pins
         currentRoll += 1
     }
 
-    func score() -> Int {
+    public func score() -> Int {
         var score = 0
         var roll = 0
         for _ in 1...10 {
