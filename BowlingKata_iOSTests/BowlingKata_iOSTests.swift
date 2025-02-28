cbd17e3999db8796d5df69ee8b90c1c9f144300a
jirving887 <jonathanirving10@gmail.com>
2025-02-28 09:59:02 -0500

    Fail spare test.

--- BowlingKata_iOSTests/BowlingKata_iOSTests.swift
+++ BowlingKata_iOSTests/BowlingKata_iOSTests.swift
@@ -96,6 +96,19 @@ struct BowlingKata_iOSTests {
         #expect(sut.frameScores == makeFrameScores(firstScore: 2, secondScore: secondFrameScore))
         #expect(sut.rollScores == ["1", "1", thirdRollScore, fourthRollScore])
     }
+    
+    //spare -> wait for next roll
+    @Test
+    func viewModel_rollSpareFirstFrame_frameScoreAddsBonus() {
+        var sut = ViewModel()
+        
+        sut.roll(9)
+        sut.roll(1)
+        sut.roll(0)
+        
+        #expect(sut.frameScores == makeFrameScores(firstScore: 10))
+        #expect(sut.rollScores == ["9", "/", "0"])
+    }
 
     func makeFrameScores(firstScore: Int? = nil, secondScore: Int? = nil) -> [Int?] {
         [firstScore, secondScore, nil, nil, nil, nil, nil, nil, nil, nil]
