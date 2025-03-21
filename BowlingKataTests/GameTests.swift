e51c3394110757d15a65191e4244873f2d447b1f
Ricky Munz <ricky.munz@gmail.com>
2025-03-21 10:26:50 -0400

    Add incorrect assertion comment

--- BowlingKataTests/GameTests.swift
+++ BowlingKataTests/GameTests.swift
@@ -110,6 +110,7 @@ final class GameTests: XCTestCase {
 
         XCTAssertEqual(game.frames[0].roll1, 4)
         XCTAssertEqual(game.frames[0].roll2, nil)
+        XCTAssertEqual(game.frames[0].score, 4) // TODO: Should be nil, score for pins requires roll2 != nil
     }
 
     func test_rollTwice_setsAndScoresFrame() {
