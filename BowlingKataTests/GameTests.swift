9ff0d23575ac1dc2615d157981f4feafe970dc94
Ricky Munz <ricky.munz@gmail.com>
2025-03-21 09:47:59 -0400

    Reorder tests

--- BowlingKataTests/GameTests.swift
+++ BowlingKataTests/GameTests.swift
@@ -56,19 +56,19 @@ final class GameTests: XCTestCase {
         XCTAssertEqual(game.frameScores, [13, 16, 16, 16, 16, 16, 16, 16, 16, 16])
     }
 
-    func testStrikeAndThenOneRollDoesNotScoreFrame() {
-        rollStrike()
-        game.roll(3)
-
-        XCTAssertEqual(game.frameScores, Array(repeating: nil, count: 10))
-    }
-
     func testOneStrikeDoesNotScoreFrame() {
         rollStrike()
 
         XCTAssertEqual(game.frameScores, Array(repeating: nil, count: 10))
         XCTAssertEqual(game.frames[0].roll1, 10)
         XCTAssertEqual(game.frames[0].roll2, nil)
+    }
+
+    func testStrikeAndThenOneRollDoesNotScoreFrame() {
+        rollStrike()
+        game.roll(3)
+
+        XCTAssertEqual(game.frameScores, Array(repeating: nil, count: 10))
     }
 
     func testStrikeAndThenTwoRollsScoresFrame() {
