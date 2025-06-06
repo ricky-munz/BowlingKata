eb1ebefc88a3279203d86bd1b8aa7e1b69e464fb
jirving887 <jonathanirving10@gmail.com>
2025-06-06 09:43:54 -0400

    Show alert at end of game

--- BowlingKata_iOS/ContentView.swift
+++ BowlingKata_iOS/ContentView.swift
@@ -77,6 +77,9 @@ struct ContentView: View {
             }
             .disabled(viewModel.isGameCompleted)
         }
+        .alert("Game over", isPresented: $viewModel.isGameCompleted) {
+            Button("OK", role: .cancel) {}
+        }
     }
 }
 
